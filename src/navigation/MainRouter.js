74996564ad0ce9f4ac80354bc13c2da973f3b826
Dien Do Van <diendv@ingok.local>
2018-07-06 00:36:43 +0700

    edit navigation

--- src/navigation/MainRouter.js
+++ src/navigation/MainRouter.js
@@ -1,17 +1,72 @@
+import React from "react"
 import {createBottomTabNavigator} from "react-navigation";
+import Ionicons from "react-native-vector-icons/Ionicons";
 
 import Home from "../components/Home";
 import Cart from "../components/Cart";
 import Me from "../components/Me";
+import {
+    View,
+    Text,
+    Dimensions,
+    StyleSheet
+} from "react-native";
+
+const styles = StyleSheet.create({
+    iconTab: {
+        borderColor: 'rgba(255, 255, 255, 0.5)',
+        width: width / 3,
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center'
+    }
+});
+
+const {width, height} = Dimensions.get('window');
+
+const IconTab = (src, tintColor, title) => (
+    <View style={{flex:1, flexDirection:'column',alignItems: 'center'}}>
+        <Ionicons size={30} color={tintColor} name={src}/>
+        <Text style={{fontSize: 14, color: tintColor, fontWeight: 'bold'}}>{title}</Text>
+    </View>
+);
 
 export default MainRouter = createBottomTabNavigator({
     Home: {
-        screen: Home
+        screen: Home,
+        navigationOptions: {
+            tabBarIcon: ({tintColor}) => IconTab('ios-home', tintColor, "Home")
+        }
     },
     Cart: {
-        screen: Cart
+        screen: Cart,
+        navigationOptions: {
+            tabBarIcon: ({tintColor}) => IconTab('ios-basket', tintColor, "Cartgfgfgfg")
+        }
     },
     Me: {
-        screen: Me
+        screen: Me,
+        navigationOptions: {
+            tabBarIcon: ({tintColor}) => IconTab('ios-person', tintColor, "Me")
+        }
     }
+}, {
+    tabBarOptions: {
+        activeTintColor: 'white',
+        inactiveTintColor: 'white',
+        activeBackgroundColor: 'red',
+        showIcon: true,
+        showLabel: false,
+        style: {
+            backgroundColor: 'blue',
+        },
+        indicatorStyle: {
+            backgroundColor: 'black',
+            height: 100
+        },
+        upperCaseLabel: false
+    },
+    animationEnabled: true,
+    lazy: true,
+    swipeEnabled: true,
 });
--- src/navigation/RootRouter.js
+++ src/navigation/RootRouter.js
@@ -3,6 +3,8 @@ import {createStackNavigator} from "react-navigation";
 import Login from "../components/Login";
 import MainRouter from "./MainRouter";
 
+
+console.disableYellowBox = true;
 export default RootRouter = createStackNavigator({
     Login: {
         screen: Login,
